bc9221d556850f480d9b4c9b8172fabb3db50a31
Hello World <lyz1995l@gmail.com>
2015-11-28 18:38:50 +0800

    File module

---
+++ python/hmmfile.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+class hmmfile:
+    def __init__(self, filename):
+        self.f = filename
+        
+    def readTraining(self):
+        result = []
+        fo = open(self.f, 'r')
+        while True:
+            line = fo.readline()
+            if(line == ''):
+                break
+            if(line == '\n'):
+                continue
+            temp = line.replace('\n','').split(' ')
+            result.append(tuple(temp))
+            
+        fo.close()
+        return result
+        
+    def readTesting(self):
+        result = []
+        fo = open(self.f, 'r')
+        while True:
+            line = fo.readline()
+            if(line == ''):
+                break
+            if(line == '\n'):
+                continue
+            result.append(line.replace('\n',''))
+            
+        fo.close()
+        return result
---
+++ python/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import os
+import sys
+sys.path.append(os.getcwd())
+
+# User defined modules
+import hmm
+from hmmfile import hmmfile
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+    main()
